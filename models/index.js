a6e3863f4c9f9f3871e8570a2302a6b0aa38904d
Jacob Guiro <jguiro09@gmail.com>
2021-07-28 16:28:42 -0500

    Models added

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'id',
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id',
+})
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(ProductTag, {
+  through: 'id'
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: 'id'
+})
 
 module.exports = {
   Product,
